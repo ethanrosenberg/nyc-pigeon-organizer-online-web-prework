a221f7ff718c61edb1b1f284888a42fa1b459665
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-13 08:11:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,28 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  pigeon_list = {}
-  
-  data.each do |key, value|
-    
-    
+  pigeon_data[:color].each do |key, value|
+    value.each do |att, attvalue|
+        if !mydata.has_key?(att)
+          mydata[att] = {}
+        end
+    end
+  end
+
+  pigeon_data[:gender].each do |key, value|
+    value.each do |att, attvalue|
+        if !mydata.has_key?(att)
+          mydata[att] = {}
+        end
+    end
+  end
+
+  pigeon_data[:lives].each do |key, value|
+    value.each do |att, attvalue|
+        if !mydata.has_key?(att)
+          mydata[att] = {}
+        end
+    end
   end
   
 end
