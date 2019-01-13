fb0b1a7fa42cd57658ce37ce8ea8e3eda21abc16
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-13 08:25:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,27 +2,35 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   
   mydata = {}
+
   
-  data[:color].each do |key, value|
+  pigeon_data[:color].each do |key, value|
+    
     value.each do |att, attvalue|
         if !mydata.has_key?(att)
-          mydata[att] = {:color => {}, :gender => {}, :lives => {}}
+          mydata[att] = {:color => [key], :gender => [], :lives => []}
+        else
+          mydata[att][:color] << key
         end
     end
   end
 
-  data[:gender].each do |key, value|
+  pigeon_data[:gender].each do |key, value|
     value.each do |att, attvalue|
         if !mydata.has_key?(att)
-          mydata[att] = {:color => {}, :gender => {}, :lives => {}}
+            mydata[att] = {:color => [], :gender => [key], :lives => []}
+        else
+            mydata[att][:gender] << key
         end
     end
   end
 
-  data[:lives].each do |key, value|
+  pigeon_data[:lives].each do |key, value|
     value.each do |att, attvalue|
         if !mydata.has_key?(att)
-          mydata[att] = {:color => {}, :gender => {}, :lives => {}}
+            mydata[att] = {:color => [], :gender => [], :lives => [key]}
+        else
+            mydata[att][:lives] << key
         end
     end
   end
